67da4b1086862d1c82179893091037d3a8767548
NitaiSalem <nitaisalem91@gmail.com>
2021-10-10 19:02:50 +0300

    changed router settings

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-       <HashRouter>
     <App />
-    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
